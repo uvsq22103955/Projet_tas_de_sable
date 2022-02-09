2afbddc49c9d77e718c0a5c5ae1e63a82dfa5bbd
uvsq22103456 <91737650+uvsq22103456@users.noreply.github.com>
2022-02-09 14:47:48 +0100

    Create Tas_de_sable.py

--- Tas_de_sable.py
+++ Tas_de_sable.py
@@ -1,7 +1,7 @@
 #########################################
 # groupe 6
-# Gaudiniere Nathan 
-# Lawson Late Patrick 
-# Said Hassani Samia
+# Gaudiniere Nathan
+# Lawson Late Patrick
+# Said Hassani Samia gbjv
 # https://github.com/uvsq22103955/Projet_tas_de_sable
 #########################################
